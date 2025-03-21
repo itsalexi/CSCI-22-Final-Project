befc814446d80b493fe7edfc5d89199d2f933c66
Kenaz <kenaz.celestino@gmail.com>
2025-03-21 14:16:33 +0800

    Base requirements

---
+++ GameCanvas.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JComponent{
+
+    public GameCanvas(){
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+
+    }
+    
+}
---
+++ GameFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class GameFrame {
+    
+    private JFrame f;
+
+    public GameFrame(){
+        f = new JFrame();
+
+        f.setTitle("Placeholder");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setVisible(true);
+    }
+}
