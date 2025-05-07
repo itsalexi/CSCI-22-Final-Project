539025319f4fbfda881ad44b0c8fb7991394052c
Kenaz <66073275+Kencel@users.noreply.github.com>
2025-05-07 21:36:17 +0800

    cleaned up files

--- WorldGenerator.java
+++ WorldGenerator.java
@@ -14,6 +14,47 @@ public class WorldGenerator {
 
   private int[] validSpawn;
 
+  private class WaterEdgeMatrix {
+
+    private Boolean[][] waterPositions;
+
+    public WaterEdgeMatrix(Boolean[][] wp) {
+        waterPositions = wp;
+    }
+
+    @Override
+    public boolean equals(Object another) {
+        if (another == this) {
+            return true;
+        }
+
+        if (another == null) {
+            return false;
+        }
+
+        if (another.getClass() != this.getClass()) {
+            return false;
+        }
+
+        WaterEdgeMatrix other = (WaterEdgeMatrix) another;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (waterPositions[i][j] == null || other.getWaterPositions()[i][j] == null) {
+                    continue;
+                }
+                if (waterPositions[i][j] != other.getWaterPositions()[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public Boolean[][] getWaterPositions() {
+        return waterPositions;
+    }
+  }
+
   public WorldGenerator(int s, int w, int h) {
     seed = s;
     width = w;
