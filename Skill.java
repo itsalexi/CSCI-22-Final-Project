c94c0a59b8edfed6b91c187b8789d77a812e335c
Kenaz <kenaz.celestino@gmail.com>
2025-05-08 12:09:13 +0800

    skillet

--- Skill.java
+++ Skill.java
@@ -4,12 +4,20 @@ public class Skill {
    
     private ArrayList<Skill> nextSkills;
     private String name;
-    private int cost;
+    private int unlockCost;
+    private int initialUpgradeCost;
+    private double scalingFactor;
+    private int level;
+    private int maxLevel;
 
-    public Skill(String n, int c, ArrayList<Skill> ns) {
+    public Skill(String n, ArrayList<Skill> ns, int unlock, int upgrade, double scaling, int max) {
         name = n;
-        cost = c;
         nextSkills = ns;
+        unlockCost = unlock;
+        initialUpgradeCost = upgrade;
+        scalingFactor = scaling;
+        level = 0;
+        maxLevel = max;
     }
 
     public ArrayList<Skill> getNextSkills() {
@@ -20,11 +28,50 @@ public class Skill {
         return name;
     }
 
-    public int getCost() {
-        return cost;
+    public int getUnlockCost() {
+        return unlockCost;
+    }
+
+    public int getUpgradeCost() {
+        return (int) (initialUpgradeCost + initialUpgradeCost * scalingFactor * level * level);
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void unlock() {
+        level = 1;
+    }
+
+    public boolean isUnlocked() {
+        return level > 0;
+    }
+
+    public void upgrade() {
+        level += 1;
     }
 
     public void addNextSkill(Skill s) {
         nextSkills.add(s);
+    }
+
+    public boolean isMaxLevel() {
+        return level == maxLevel;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+
+        Skill other = (Skill) obj;
+        return name.equals(other.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
--- SkillTreeSystem.java
+++ SkillTreeSystem.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 public class SkillTreeSystem {
 
     private Skill rootSkill;
     private ArrayList<Skill> skills;
+    private Set<Skill> unlocked;
+    private EconomySystem economySystem;
 
-    public SkillTreeSystem(ArrayList<Skill> s) {
+    public SkillTreeSystem(ArrayList<Skill> s, EconomySystem ec) {
        skills = s;
        rootSkill = s.get(0);
+       unlocked = new HashSet<>();
+       unlocked.add(rootSkill);
+       economySystem = ec;
     }
 
     public ArrayList<Skill> getSkillsAtLevel(int level) {
@@ -28,6 +35,35 @@ public class SkillTreeSystem {
 
     public int getTreeHeight() {
         return (int) (Math.log(skills.size() + 1) / Math.log(2)) - 1;
+    }
+
+    public void unlockSkill(Skill s) {
+        if (unlocked.contains(s)) {
+            return;
+        }
+
+        if (economySystem.getSkillPoints() < s.getUnlockCost()) {
+            return;
+        }
+
+        for (Skill currSkill : unlocked) {
+            System.out.println("a");
+            for (Skill unlockable : currSkill.getNextSkills()) {
+                if (s.equals(unlockable)) {
+                    unlocked.add(s);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void upgradeSkill(Skill s) {
+        
+    
+
+        if (unlocked.contains(s)) {
+
+        }
     }
     
 }
